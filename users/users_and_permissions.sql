dc37360a6d5152bf0f92ef7332d3f10dffadcb01
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-13 20:11:56 -0600

    feat(users) agregar usuarios 3 y 4

--- users/users_and_permissions.sql
+++ users/users_and_permissions.sql
@@ -17,3 +17,23 @@ TO 'cramirez'@'localhost';
 CREATE USER IF NOT EXISTS 'mlopez'@'localhost' IDENTIFIED BY 'Maria2026';
 GRANT ALL PRIVILEGES ON distribuidora_de_gaseosas_del_valle.* 
 TO 'mlopez'@'localhost';
+
+-- ==========================================
+-- 3. Usuario para José Miguel Hernández (Sede Sur Escuintla)
+-- ==========================================
+CREATE USER IF NOT EXISTS 'jhernandez'@'localhost' IDENTIFIED BY 'Jose2026';
+GRANT ALL PRIVILEGES ON distribuidora_de_gaseosas_del_valle.*
+TO 'jhernandez'@'localhost';
+
+-- ==========================================
+-- 4. Usuario para Ana Lucía Morales (Sede Norte Cobán)
+-- ==========================================
+CREATE USER IF NOT EXISTS 'amorales'@'localhost' IDENTIFIED BY 'Ana2026';
+GRANT ALL PRIVILEGES ON distribuidora_de_gaseosas_del_valle.*
+TO 'amorales'@'localhost';
+
+-- Ver los permisos de cada usuario
+SHOW GRANTS FOR 'cramirez'@'localhost';
+SHOW GRANTS FOR 'mlopez'@'localhost';
+SHOW GRANTS FOR 'jhernandez'@'localhost';
+SHOW GRANTS FOR 'amorales'@'localhost';

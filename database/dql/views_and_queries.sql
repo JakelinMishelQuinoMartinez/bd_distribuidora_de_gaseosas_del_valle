1cb372a75febb58a416d5da94b783dba008d7d58
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-13 19:38:25 -0600

    feat(dql): consultar un listado de productos más vendidos

--- database/dql/views_and_queries.sql
+++ database/dql/views_and_queries.sql
@@ -16,3 +16,14 @@ WHERE stock_actual < stock_minimo;
 SELECT * 
 FROM pedidos 
 WHERE fecha BETWEEN '2026-08-10' AND '2026-08-13'; 
+
+-- ===================================================================================
+-- 3. Listar los productos más vendidos (con JOIN y GROUP BY).
+-- ===================================================================================
+SELECT p.nombre AS producto,
+    SUM(dp.cantidad) AS total_vendido
+FROM detalles_pedidos dp
+JOIN productos p ON dp.id_producto = p.id
+GROUP BY p.id, p.nombre
+ORDER BY total_vendido DESC
+LIMIT 10;

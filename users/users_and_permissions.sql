4f86fa749cb1dec26c551db5722229b36fcc47b8
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-13 20:11:33 -0600

    feat(users) agregar 2 usuarios

--- users/users_and_permissions.sql
+++ users/users_and_permissions.sql
@@ -1,1 +1,19 @@
 
+USE distribuidora_de_gaseosas_del_valle;
+-- ====================================
+-- USUARIOS
+-- ====================================
+
+-- ==========================================
+-- 1. Usuario para Carlos Eduardo Ramírez (Sede Central Guatemala)
+-- ==========================================
+CREATE USER IF NOT EXISTS 'cramirez'@'localhost' IDENTIFIED BY 'Carlos2026';
+GRANT ALL PRIVILEGES ON distribuidora_de_gaseosas_del_valle.* 
+TO 'cramirez'@'localhost';
+
+-- ==========================================
+-- 2. Usuario para María Fernanda López (Sede Occidente Quetzaltenango)
+-- ==========================================
+CREATE USER IF NOT EXISTS 'mlopez'@'localhost' IDENTIFIED BY 'Maria2026';
+GRANT ALL PRIVILEGES ON distribuidora_de_gaseosas_del_valle.* 
+TO 'mlopez'@'localhost';

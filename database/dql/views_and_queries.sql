f800da81b56a53831a6b65a72d7bd0ed2f262fcb
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-13 19:38:56 -0600

    feat(dql): consultar clientes y la contidad de pedidos realizados

--- database/dql/views_and_queries.sql
+++ database/dql/views_and_queries.sql
@@ -27,3 +27,14 @@ JOIN productos p ON dp.id_producto = p.id
 GROUP BY p.id, p.nombre
 ORDER BY total_vendido DESC
 LIMIT 10;
+
+-- ===================================================================================
+-- 4. Mostrar clientes y la cantidad de pedidos realizados.
+-- ===================================================================================
+SELECT 
+    c.nombre_completo AS cliente,
+    COUNT(p.id) AS cantidad_pedidos
+FROM clientes c
+LEFT JOIN pedidos p ON c.id = p.id_cliente
+GROUP BY c.id, c.nombre_completo
+ORDER BY cantidad_pedidos DESC;

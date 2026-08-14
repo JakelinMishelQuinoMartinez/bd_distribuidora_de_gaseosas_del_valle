1d9af74437b3de7d33527cb42cd6bc4ddfd354fc
JakelinMishelQuinoMartinez <quinojakelin@gmail.com>
2026-08-13 19:26:59 -0600

    feat(dml): insertar datos a la tabla categorias

---
+++ database/dml/inserts.sql
@@ -0,0 +1,11 @@
+USE distribuidora_de_gaseosas_del_valle;
+
+-- ======================================================
+-- 1. CATEGORÍAS
+-- ======================================================
+INSERT INTO categorias (nombre, descripcion) VALUES
+('Cola', 'Gaseosas sabor cola tradicional y dietéticas'),
+('Frutales', 'Gaseosas con sabores a frutas como naranja, limón, uva'),
+('Energéticas', 'Bebidas con cafeína y taurina para energía'),
+('Cítricas', 'Gaseosas con base de cítricos como lima-limón y pomelo'),
+('Light/Zero', 'Gaseosas sin azúcar o bajas en calorías');
